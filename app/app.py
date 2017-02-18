3a83a16b2d331999cc7bbbe13a7be0e7f190ab5e
Ethan Trinh <etonxt@gmail.com>
2017-02-18 03:47:14 -0500

    cleaner web pages

--- app/app.py
+++ app/app.py
@@ -5,26 +5,36 @@ import database
 
 app = Flask(__name__)
 
+#home page
+#group code inputted here
 @app.route('/')
 def landing():
     return render_template('landing.html')
 
+#login page
+#needs to verify user and password validity
 @app.route('/login/', methods= ['GET', 'POST'])
 def login():
     return render_template('login.html')
 
+#page when group code entered
+#needs to verify groupcode
+#should display group name, queue, group members
 @app.route('/group/', methods= ['GET', 'POST'])
 def group():
     if request.method=='POST':
         groupcode = request.form['groupcode']
         return render_template('group.html')
 
+#page for user profile
+#should display user's groups
 @app.route('/profile/', methods= ['GET', 'POST'])
 def user_profile():
     name = request.cookies.get('name')
     pw = request.cookies.get('pw')
     return render_template('profile.html', name = name)
 
+#cookie is set after sign up or log in
 @app.route('/setcookie/', methods=['POST', 'GET'])
 def setcookie():
     if request.method=='POST':
@@ -35,6 +45,8 @@ def setcookie():
         resp.set_cookie('pw', pw)
         return redirect('/profile/')
 
+#signup page
+#needs to verify if user already exists
 @app.route('/signup/')
 def signup():
     return render_template('signup.html')
